9a1ccb690b2e17cc49e78c34b573abee00616f9c
sumin <mins19960091@gmail.com>
2024-12-04 20:44:59 +0900

    메인페이지 레이아웃 생성, 아이템 페이지 페이징 처리

--- src/main/java/hello/lol/home/HomeController.java
+++ src/main/java/hello/lol/home/HomeController.java
@@ -2,7 +2,6 @@ package hello.lol.home;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -11,8 +10,26 @@ public class HomeController {
     public String home(){
         return "/login";
     }
+
     @PostMapping("/main")
     public String main(){
         return "/main";
+    }
+
+    @GetMapping("/items/items")
+    public String items(){
+        return "/items/items";
+    }
+    @GetMapping("/items/newItems")
+    public String newItems(){
+        return "/items/newItems";
+    }
+    @GetMapping("/items/bestItem")
+    public String bestItem(){
+        return "/items/bestItem";
+    }
+    @GetMapping("/items/counterItem")
+    public String counterItem(){
+        return "/items/counterItem";
     }
 }
