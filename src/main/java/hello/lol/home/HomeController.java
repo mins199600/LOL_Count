3fed77a00f2be51d97ca6e176b6ffe0b859202de
sumin <mins19960091@gmail.com>
2024-12-09 16:34:10 +0900

    공지사항 조회 기능 구현

--- src/main/java/hello/lol/home/HomeController.java
+++ src/main/java/hello/lol/home/HomeController.java
@@ -20,16 +20,20 @@ public class HomeController {
     public String items(){
         return "/items/items";
     }
+
     @GetMapping("/items/newItems")
     public String newItems(){
         return "/items/newItems";
     }
+
     @GetMapping("/items/bestItem")
     public String bestItem(){
         return "/items/bestItem";
     }
+
     @GetMapping("/items/counterItem")
     public String counterItem(){
         return "/items/counterItem";
     }
+
 }
