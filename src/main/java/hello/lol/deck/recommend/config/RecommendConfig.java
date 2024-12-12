95e19f36f3b110ff647d16d0f8d3bcd3f36f460e
sumin <mins19960091@gmail.com>
2024-12-12 15:08:14 +0900

    추천 덱 기능 구현중 버그 발생

---
+++ src/main/java/hello/lol/deck/recommend/config/RecommendConfig.java
@@ -0,0 +1,38 @@
+package hello.lol.deck.recommend.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import javax.sql.DataSource;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class RecommendConfig {
+
+    private final DataSource dataSource;
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setMapperLocations(
+                new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml"));
+        return sessionFactory.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
---
+++ src/main/java/hello/lol/deck/recommend/repository/DeckDto.java
@@ -0,0 +1,32 @@
+package hello.lol.deck.recommend.repository;
+
+public class DeckDto {
+
+    private int deckId;
+    private String deckName;
+    private String championNames;
+
+    public int getDeckId() {
+        return deckId;
+    }
+
+    public void setDeckId(int deckId) {
+        this.deckId = deckId;
+    }
+
+    public String getDeckName() {
+        return deckName;
+    }
+
+    public void setDeckName(String deckName) {
+        this.deckName = deckName;
+    }
+
+    public String getChampionNames() {
+        return championNames;
+    }
+
+    public void setChampionNames(String championNames) {
+        this.championNames = championNames;
+    }
+}
