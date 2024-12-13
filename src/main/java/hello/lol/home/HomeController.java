ccc4b219f24d045c8dc366b990c9d0ca0c185638
sumin <mins19960091@gmail.com>
2024-12-13 18:19:21 +0900

    아이템 백엔드 설계로 인해 컨트롤러 빌드 추가

--- src/main/java/hello/lol/home/HomeController.java
+++ src/main/java/hello/lol/home/HomeController.java
@@ -17,24 +17,4 @@ public class HomeController {
         return "/main";
     }
 
-    @GetMapping("/items/items")
-    public String items(){
-        return "/items/items";
-    }
-
-    @GetMapping("/items/newItems")
-    public String newItems(){
-        return "/items/newItems";
-    }
-
-    @GetMapping("/items/bestItem")
-    public String bestItem(){
-        return "/items/bestItem";
-    }
-
-    @GetMapping("/items/counterItem")
-    public String counterItem(){
-        return "/items/counterItem";
-    }
-
 }
